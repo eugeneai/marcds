f44c825fa39da2e74de7393b8d9564373ad86b92
Irina Gomboeva <iregomboeva@mail.ru>
2016-12-23 05:55:39 +0000

    Added the stub of searcher.

--- marc_digital_storage/views.py
+++ marc_digital_storage/views.py
@@ -3,4 +3,10 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/page-fullwidth.pt')
 def my_view(request):
-    return {'project': 'marc-digital-storage-11166', 'name':'V.I.Lenin'}
+    if request.method == "POST":
+        # Найти книгу
+        result="not found"
+    else:
+        result = None
+    request.POST
+    return {'project': 'Цифровой архив ИДСТУ СО РАН', "result":result}
